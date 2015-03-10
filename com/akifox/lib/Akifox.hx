79fed6b03f4f73b7b6980895bf87747ae0aae92e
yupswing <simonecingano@gmail.com>
2015-03-10 02:24:08 +0100

    uid generator

--- com/akifox/lib/Akifox.hx
+++ com/akifox/lib/Akifox.hx
@@ -637,6 +637,18 @@ class Akifox
 		prefData.write(name,value);
 	}
 
+	private static var UID_CHARS = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+	public static function randomUID(?size:Int=32):String
+	{
+		var nchars = UID_CHARS.length;
+		var uid = new StringBuf();
+		for (i in 0 ... size){
+			uid.addChar(UID_CHARS.charCodeAt( Std.random(nchars) ));
+		}
+		return uid.toString();
+	}
+
 	//##########################################################################################
 	//
 	// OTHERS
